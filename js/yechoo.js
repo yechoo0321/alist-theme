f0acbc9aee1fa45d83894988f2f859eab8761b73
yechoo0321 <64009001+yechoo0321@users.noreply.github.com>
2022-05-16 23:51:18 +0800

    Update yechoo.js

--- js/yechoo.js
+++ js/yechoo.js
@@ -1,14 +1,14 @@
 function ref() {
 	const xhr = new XMLHttpRequest();
-	const url = 'https://saying.api.azwcl.com/saying/get'
+	const url = 'http://' + window.location.host + '/txt/api.php'
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			const json = JSON.parse(xhr.responseText)
 			const data = json.data
 			let down = document.getElementById("down_click");
 			// console.log(down);
-			down.innerHTML = data.content;
-			console.log(data.content);
+			down.innerHTML = data;
+			console.log(data);
 		}
 	}
 	xhr.open('GET', url)
